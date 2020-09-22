c7d4cce9c7e606443e1e671889f35b5f30b7c3c2
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-22 11:58:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,63 +1,125 @@
 // Your code here
+//sorry ! , I didn't read the curriculum for this lab,i just used test questions, and i was passing all of them in using map and reduce
+// , after I reviewed this lab, I realized that and did this lab  again using map-like and reduce-like
 function mapToNegativize(sourceArray) {
 
-    return sourceArray.map(el =>
-        el * -1
-    )
+  //  return sourceArray.map(el =>
+    //    el * -1
+  //  )
+  let newArray=[]
+  sourceArray.forEach (el =>
 
+    newArray.push(el * -1)
+  )
+return newArray
 }
 
 
 function mapToNoChange(sourceArray) {
 
-    return sourceArray.map(el => el)
+    let newArray=[]
+    sourceArray.forEach (el =>
+
+      newArray.push(el)
+    )
+  return newArray
 }
 
 function mapToDouble(sourceArray) {
 
+    //return sourceArray.map(el => el * 2)
+    let newArray=[]
+    sourceArray.forEach (el =>
+      newArray.push(el * 2)
+    )
+  return newArray
 
-    return sourceArray.map(el => el * 2)
 }
 
 function mapToSquare(sourceArray) {
 
+    //return sourceArray.map(element => element * element)
 
-    return sourceArray.map(element => element * element)
+    let newArray=[]
+    sourceArray.forEach (el =>
+      newArray.push(el * el)
+    )
+  return newArray
 }
 
 
-function reduceToTotal(sourceArray, startingPoint = 0) {
-    return sourceArray.reduce(function(total, element) { return element + total }, startingPoint)
+function reduceToTotal(sourceArray, startingPoint) {
+    //return sourceArray.reduce(function(total, element) { return element + total }, startingPoint)
+
+let total
+ if (startingPoint) {
+
+  total=startingPoint
+   for(let i=0 ;i< sourceArray.length;i++)
+   {
+     total +=sourceArray[i]
+   }
+
+ }
+ else
+ {
+
+  total=0
+   for(let i=0 ;i< sourceArray.length;i++)
+   {
+     total +=sourceArray[i]
+   }
+ }
+ return total
 
 }
 
 
 function reduceToAllTrue(sourceArray) {
-    let truethy = true
-    sourceArray.reduce(function(total, element) {
-        if (element === false) {
-            truethy = false
-        }
-    })
+    //let truethy = true
+  ///  sourceArray.reduce(function(total, element) {
+    //    if (element === false) {
+      //      truethy = false
+    //    }
+  //  })
 
-    if (truethy === true) {
-        return true
+    //if (truethy === true) {
+    //    return true
 
-    } else {
+//    } else {
 
-        return false
+  //      return false
+  //  }
+
+    for(let i=0 ;i< sourceArray.length;i++)
+    {
+          if (sourceArray[i] === false ){
+            return false
+          }
+
     }
+    return true
 
 
 }
 
 function reduceToAnyTrue(sourceArray) {
-    let result = false
-    result = sourceArray.reduce(function(total, element) {
-        if (element === true) {
-            return true
-        }
+  //  let result = false
+  //  result = sourceArray.reduce(function(total, element) {
+    //    if (element === true) {
+      //      return true
+      //  }
+  //  })
+  //  return result === true ? true : false
 
-    })
-    return result === true ? true : false
+  for(let i=0 ; i < sourceArray.length ; i++)
+  {
+      if (sourceArray[i] === true ){
+          return true
+      }
+
+  }
+  return false
+
+
 }
